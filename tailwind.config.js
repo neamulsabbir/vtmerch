30a7761aa9423682e552c2a11252828c715f782d
neamulsabbir <neamulkabir97@gmail.com>
2024-10-05 18:05:48 +0600

    customize css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '340px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+        '3xl': '1750px',
+        'custom-home': '450px'
+      },
+      fontFamily: {
+        'actOfRejection': ['"Act of Rejection"', 'sans-serif'],
+        'montserrat': ['Montserrat', 'sans-serif'],
+        'sans': ['Inter var', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
