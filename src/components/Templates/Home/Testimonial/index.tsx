a42aa8003ca61d8af8e8aa0fef4fa3265c1468f0
NeamulSabbir97 <neamul.kabir@zdrop.com.bd>
2024-10-06 08:06:01 -0400

    testimonial design done

---
+++ src/components/Templates/Home/Testimonial/index.tsx
@@ -0,0 +1,35 @@
+export const Testimonial = () => {
+	return (
+		<section className="bg-gradient-to-r from-[#EC3339] to-[#BB1117] relative px-[50px] lg:px-[150px]">
+			<div className="flex justify-between items-center flex-wrap gap-6 md:gap-10 xl:gap-0">
+				<div className="flex-grow lg:max-w-screen-md xl:max-w-[860px] lg:pt-20 lg:pb-10 xl:py-40 px-6 md:px-0">
+					<div className="absolute top-[2%] md:top-[2%] md:left-[6%] xl:top-[4%] ">
+						<img
+							src="/images/img/quotes.svg"
+							alt="Mission Image"
+							width={180}
+							height={180}
+							className="w-[4rem] md:w-[120px] lg:w-[160px] xl:w-[210px] "
+						/>
+					</div>
+
+					<p className="text-xl md:text-3xl lg:text-4xl font-bold  pt-10 md:pt-16 lg:pt-2 font-montserrat text-white leading-[350px]">
+						OUR MISSION IS TO ENLIGHTEN, ENTERTAIN AND EMPOWER CURRENT AND FUTURE LEADERS AROUND THE WORLD.
+					</p>
+				</div>
+
+				<div className="static mx-auto xl:absolute bottom-0 right-[5%] overflow-hidden sm:overflow-visible">
+					<img
+						src="/images/img/men.png"
+						alt="Elevated Image"
+						width={300}
+						height={300}
+						className="min-h-full sm:h-[20rem] xl:h-[35.5rem] w-full "
+					/>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+//
