11ef2a15e5f08030f6e5433c147e8b1447b9e4df
NeamulSabbir97 <neamul.kabir@zdrop.com.bd>
2024-10-06 08:05:43 -0400

    created main layout

--- src/components/SharedFolder/LayOuts/MainLayout.tsx
+++ src/components/SharedFolder/LayOuts/MainLayout.tsx
@@ -1,9 +1,17 @@
+import { FC } from "react";
+import { Footer } from "../Footer";
+import { Navbar } from "../Navbar";
 
-
-export const MainLayout = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+export const MainLayout: FC<PropsType> = ({ children }) => {
+	return (
+		<>
+			<Navbar />
+			{children}
+			<Footer />
+		</>
+	);
 };
+
+interface PropsType {
+	children?: React.ReactNode;
+}
