0da3e5eb8260d82cf33ddb5d3c989d54476ce440
NeamulSabbir97 <neamul.kabir@zdrop.com.bd>
2024-10-06 08:04:11 -0400

    customize color and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      screens: {
-        'xs': '340px',
-        'sm': '640px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-        '2xl': '1536px',
-        '3xl': '1750px',
-        'custom-home': '450px'
-      },
-      fontFamily: {
-        'actOfRejection': ['"Act of Rejection"', 'sans-serif'],
-        'montserrat': ['Montserrat', 'sans-serif'],
-        'sans': ['Inter var', 'sans-serif'],
-      },
-    },
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			screens: {
+				xs: "340px",
+				sm: "640px",
+				md: "768px",
+				lg: "1024px",
+				xl: "1280px",
+				"2xl": "1536px",
+				"3xl": "1750px",
+				"custom-home": "450px",
+			},
+			fontFamily: {
+				actOfRejection: ['"Act of Rejection"', "sans-serif"],
+				montserrat: ["Montserrat", "sans-serif"],
+				sans: ["Inter var", "sans-serif"],
+			},
+			colors: {
+				bgRed: "#EA2127",
+				bgLightGray: "#F6F5F5",
+				fontBlack: "#060606",
+			},
+		},
+	},
+	plugins: [],
+};
