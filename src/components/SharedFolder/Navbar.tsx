1c1e928655440f2d42eb9319f905c0dae93a2f7f
NeamulSabbir97 <neamul.kabir@zdrop.com.bd>
2024-10-06 08:07:52 -0400

    navbar deishn done

--- src/components/SharedFolder/Navbar.tsx
+++ src/components/SharedFolder/Navbar.tsx
@@ -1,9 +1,14 @@
-
+import { Brand } from "./Brand";
 
 export const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+	return (
+		<nav className="bg-bgRed">
+			<div className="flex justify-between items-center px-10 md:px-20 py-10">
+				<Brand isWhite />
+				<div>
+					<button className="text-white text-3xl sm:text-4xl ">&#9776;</button>
+				</div>
+			</div>
+		</nav>
+	);
 };
